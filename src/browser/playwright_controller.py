ae30561c18382c256326338b1fbd2d514bf62a75
khushwant04 <khushwantsanwalot2004@outlook.com>
2025-03-24 19:08:54 +0000

    Implement basic Playwright functionality to launch browser and navigate to a URL

--- src/browser/playwright_controller.py
+++ src/browser/playwright_controller.py
@@ -1,1 +1,11 @@
-# playwright_contorller
+# Playwrite
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    
+    page.goto("https://hexelstudio.com")
+    input("Press enter to exit...")
+    browser.close()
